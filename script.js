6d69e6360caf1926d051881c3fc8b871d2b58288
akhila841301-boop <akhila841301@gmail.com>
2026-08-11 11:19:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ function loadSong(index, autoplay=false){
   currentIndex = (index + songs.length) % songs.length;
   const song = songs[currentIndex];
 
-  audio.src = encodeURI("./" + song.file);
+  audio.src = "./" + song.file.split("/").map(encodeURIComponent).join("/");
   audio.load();
   songTitle.textContent = song.title;
   songArtist.textContent = song.artist || "Unknown Artist";
