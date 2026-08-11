ed041ceda03e51a57fe53dcc55942eb635bf7027
Aarif <mdaarifa825@gmail.com>
2026-08-11 23:31:31 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -20,77 +20,77 @@
 
   {
     title: "Ek Dil Hai",
-    artist: "90s Yaadein",
+    artist: "Kumar Sanu & Alka Yagnik",
     file: "Ek Dil Hai - song-001.mp3",
     duration: "--:--"
   },
 
   {
     title: "Dilbar Dilbar",
-    artist: "90s Yaadein",
+    artist: "Alka Yagnik",
     file: "Dilbar Dilbar - song-002.mp3",
     duration: "--:--"
   },
 
   {
     title: "Kitaben Bahut Si",
-    artist: "90s Yaadein",
+    artist: "Asha Bhosle & Vinod Rathod",
     file: "kitaben-bahut-si.mp3",
     duration: "--:--"
   },
 
   {
     title: "Baazigar O Baazigar",
-    artist: "90s Yaadein",
+    artist: "Kumar Sanu & Alka Yagnik",
     file: "baazigar-o-baazigar.mp3",
     duration: "--:--"
   },
 
   {
     title: "Samajh Kar Chand Jis Ko",
-    artist: "90s Yaadein",
+    artist: "Vinod Rathod & Alka Yagnik",
     file: "samajh-kar-chand-jis-ko.mp3",
     duration: "--:--"
   },
 
   {
     title: "Chhupana Bhi Nahin Aata",
-    artist: "90s Yaadein",
+    artist: "Pankaj Udhas",
     file: "chhupana-bhi-nahin-aata.mp3",
     duration: "--:--"
   },
 
   {
     title: "Dheere Dheere Pyar Ko",
-    artist: "90s Yaadein",
+    artist: "Kumar Sanu & Alka Yagnik",
     file: "dheere-dheere-pyar-ko.mp3",
     duration: "--:--"
   },
 
   {
     title: "Tumse Milne Ko Dil",
-    artist: "90s Yaadein",
+    artist: "Kumar Sanu & Alka Yagnik",
     file: "tumse-milne-ko-dil.mp3",
     duration: "--:--"
   },
 
   {
     title: "Maine Pyar Tumhi Se Kiya Hai",
-    artist: "90s Yaadein",
+    artist: "Kumar Sanu & Anuradha Paudwal",
     file: "maine-pyar-tumhi-se-kiya-hai.mp3",
     duration: "--:--"
   },
 
   {
     title: "Der Na Ho Jaye Kahin",
-    artist: "90s Yaadein",
+    artist: "Lata Mangeshkar, Suresh Wadkar, Mohammad Sayeed, Farid Sabri & Satish",
     file: "der-na-ho-jaye-kahin.mp3",
     duration: "--:--"
   },
 
   {
-    title: "Mouka Milega To Hum",
-    artist: "90s Yaadein",
+    title: "Mouka Milenga To Hum",
+    artist: "Udit Narayan & Alka Yagnik",
     file: "mouka-milenga-to-hum.mp3",
     duration: "--:--"
   }
