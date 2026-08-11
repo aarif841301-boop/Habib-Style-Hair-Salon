7729308bb816ee443cb8d085c881e76fae3492a0
Aarif <mdaarifa825@gmail.com>
2026-08-11 22:56:57 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -17,17 +17,82 @@
   1000+ songs are supported by this player.
 */
   const songs = [
+
   {
     title: "Ek Dil Hai",
     artist: "90s Yaadein",
     file: "Ek Dil Hai - song-001.mp3",
     duration: "--:--"
   },
+
   {
     title: "Dilbar Dilbar",
     artist: "90s Yaadein",
     file: "Dilbar Dilbar - song-002.mp3",
     duration: "--:--"
   },
+
+  {
+    title: "Kitaben Bahut Si",
+    artist: "90s Yaadein",
+    file: "kitaben-bahut-si.mp3",
+    duration: "--:--"
+  },
+
+  {
+    title: "Baazigar O Baazigar",
+    artist: "90s Yaadein",
+    file: "baazigar-o-baazigar.mp3",
+    duration: "--:--"
+  },
+
+  {
+    title: "Samajh Kar Chand Jis Ko",
+    artist: "90s Yaadein",
+    file: "samajh-kar-chand-jis-ko.mp3",
+    duration: "--:--"
+  },
+
+  {
+    title: "Chhupana Bhi Nahin Aata",
+    artist: "90s Yaadein",
+    file: "chhupana-bhi-nahin-aata.mp3",
+    duration: "--:--"
+  },
+
+  {
+    title: "Dheere Dheere Pyar Ko",
+    artist: "90s Yaadein",
+    file: "dheere-dheere-pyar-ko.mp3",
+    duration: "--:--"
+  },
+
+  {
+    title: "Tumse Milne Ko Dil",
+    artist: "90s Yaadein",
+    file: "tumse-milne-ko-dil.mp3",
+    duration: "--:--"
+  },
+
+  {
+    title: "Maine Pyar Tumhi Se Kiya Hai",
+    artist: "90s Yaadein",
+    file: "maine-pyar-tumhi-se-kiya-hai.mp3",
+    duration: "--:--"
+  },
+
+  {
+    title: "Der Na Ho Jaye Kahin",
+    artist: "90s Yaadein",
+    file: "der-na-ho-jaye-kahin.mp3",
+    duration: "--:--"
+  },
+
+  {
+    title: "Mouka Milega To Hum",
+    artist: "90s Yaadein",
+    file: "mouka-milenga-to-hum.mp3",
+    duration: "--:--"
+  }
 
 ];
